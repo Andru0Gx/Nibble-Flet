fc6adec3c032873a02509a43d61c177800c85c7c
Andru0Gx <Andy20ogx@gmail.com>
2023-11-24 20:51:35 -0400

    Add new page for students

--- modules/section_manager.py
+++ modules/section_manager.py
@@ -3,6 +3,7 @@
 # Libraries
 import flet as ft
 
+#* ------------------ Home Page ------------------ *#
 def def_home(page: ft.Page):
     '''
     Create the home page
@@ -16,6 +17,7 @@ def def_home(page: ft.Page):
     from Pages.App_Layout.Body_Sections.home import Home
     return Home(page)
 
+#* ------------------ Events Page ------------------ *#
 def def_events(page: ft.Page):
     '''
     Returns the result of calling the 'maintwo' function from the 'events' 
@@ -30,3 +32,19 @@ def def_events(page: ft.Page):
     '''
     from Pages.App_Layout.Body_Sections.events import maintwo
     return maintwo(page)
+
+#* ------------------ Students Page ------------------ *#
+def def_students(page: ft.Page):
+    '''
+    Returns the result of calling the 'maintwo' function from the 'students' 
+    module in the 'Pages.App_Layout.Body_Sections' package with the given 'page' 
+    object as an argument.
+    
+    Args:
+        page (ft.Page): A 'ft.Page' object representing a page.
+    
+    Returns:
+        The result of calling the 'maintwo' function with the 'page' object as an argument.
+    '''
+    from Pages.App_Layout.Body_Sections.students import Students
+    return Students(page)
