857b7bf06af495aa0df529e6420d2427f9c45e61
Andru0Gx <Andy20ogx@gmail.com>
2023-12-29 00:08:17 -0400

    Add handle window close event

--- nibble.py
+++ nibble.py
@@ -23,12 +23,14 @@ import modules.page_manager as pm
 
 # Database
 from DB.Functions.tables import create_tables as db
+from DB.Functions.temp_data_db import delete_tempdata_db
 
 
 #^ ------------------ RUN APP ------------------ ^#
 
 #TODO - Add Maximized window True
 #TODO - cambiar los botones de editar por guardar
+#TODO - Add try and except to all the functions and methods in the app (Log the errors)
 def main(page: ft.Page):
     '''Main function to run the app'''
     page.title = 'Nibble'
@@ -54,6 +56,16 @@ def main(page: ft.Page):
 
     # page.window_min_height = 768
     # page.window_min_width = 1366
+
+    def onclose(e):
+        '''Function to run on close window'''
+        if e.data == 'close':
+            delete_tempdata_db()
+            page.window_destroy()
+
+    # on close window
+    page.window_prevent_close = True
+    page.on_window_event = lambda e: onclose(e)
 
     db()
     page.update()
