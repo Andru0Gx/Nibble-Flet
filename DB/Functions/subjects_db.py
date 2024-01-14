5845759b1507ff0993eeb87f4ef0913705cc1218
Andru0Gx <Andy20ogx@gmail.com>
2024-01-14 00:11:39 -0400

    Add subject filtering and search functions to subjects_db.py

--- DB/Functions/subjects_db.py
+++ DB/Functions/subjects_db.py
@@ -79,3 +79,70 @@ def update_subject(subject_id, subject_name, phase):
         """UPDATE materia SET nombre = ?, etapa = ? WHERE id_m = ?;""", (subject_name, phase,subject_id))
     conexion.commit()
     conexion.close()
+
+#* ------------------ FILTER SUBJECTS BY PHASE ------------------ *#
+def filter_subjects(phase, phase_type):
+    '''Filter the subjects by phase'''
+    # Connect to the database
+    conexion = sqlite3.connect('DB/Nibble.db')
+    cursor = conexion.cursor()
+
+    cursor.execute(
+        """SELECT id_m,nombre, etapa FROM materia WHERE etapa = ?;""", (phase,))
+    search = cursor.fetchall()
+
+    subject = {
+        'ID': None,
+        'Nombre': None,
+        'Etapa': None
+    }
+
+    subjects_list = []
+
+    for i in search:
+        subject['ID'] = i[0]
+        subject['Nombre'] = i[1]
+        subject['Etapa'] = i[2]
+        subjects_list.append(subject.copy())
+
+    # filtrar etapas por el nombre type (liceo, colegio)
+    cursor.execute(
+        """SELECT id_m,nombre, etapa FROM materia WHERE etapa = ?;""", (phase_type,))
+    search = cursor.fetchall()
+    conexion.close()
+
+    for i in search:
+        subject['ID'] = i[0]
+        subject['Nombre'] = i[1]
+        subject['Etapa'] = i[2]
+        subjects_list.append(subject.copy())
+
+    # Ordenar la lista de materias en orden ascendente
+    subjects_list.sort(key=lambda x: x['ID'])
+
+    return subjects_list
+
+
+#* ------------------ SEARCH SUBJECT BY ID ------------------ *#
+def search_subject_by_id(subject_id):
+    '''Search a subject by id'''
+    # Connect to the database
+    conexion = sqlite3.connect('DB/Nibble.db')
+    cursor = conexion.cursor()
+
+    cursor.execute(
+        """SELECT id_m,nombre, etapa FROM materia WHERE id_m = ?;""", (subject_id,))
+    search = cursor.fetchone()
+    conexion.close()
+
+    subject = {
+        'ID': None,
+        'Nombre': None,
+        'Etapa': None
+    }
+
+    subject['ID'] = search[0]
+    subject['Nombre'] = search[1]
+    subject['Etapa'] = search[2]
+
+    return subject
